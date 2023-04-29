2aa2da40e25c3313c5bf5683e880764f74eb6f03
Mohamad Amgad <mohamadamgad09@gmail.com>
2023-04-29 04:55:11 +0300

    FVM added - contact page dark theme modified.

---
+++ green_egypt/Green_Egypt-main/green_egypt/lib/services/Shared_preferences/shared_preferences_class.dart
@@ -0,0 +1,22 @@
+import 'package:green_egypt/config/localization/language_box.dart';
+import 'package:green_egypt/services/Shared_preferences/first_launch.dart';
+import 'package:green_egypt/services/boxes/user_data_db.dart';
+
+class SharedPreferencesClass {
+  static Future<void> initAllSharedPreferences() async {
+    // initialize application theme box .
+    // await ApplicationThemeController.instance
+    //     .initApplicationThemeController_ThemeAndBox();
+
+
+    /**
+     * init language box to retrieve last stored language on application .
+     */
+    await LanguageBox.instance.initLanguageBox();
+    // user Data Box that contain all user data stored in .
+    // boxes are local storing objects as Shared Preferences put with extra speed .
+    UserDataBox.instance.initiateUserBox();
+    // initialize First launch Shared pref to decide Navigation Destination .
+    await FirstLaunch.init();
+  }
+}
