b52092dbae6db15238ae700433058fcb08ef8667
Mohamad Amgad <mohamadamgad09@gmail.com>
2023-05-12 01:16:03 +0300

    transactions page animate and refactored , start work on qrcode scanning

--- green_egypt/lib/screens/home_page/transactions_page/widgets/bar_model.dart
+++ green_egypt/lib/screens/home_page/transactions_page/widgets/bar_model.dart
@@ -1,27 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:green_egypt/config/dimensions.dart';
 
-class BarModel extends StatelessWidget {
+class BarModel extends StatefulWidget {
   BarModel({required this.barText, required this.barHeight});
   late String barText;
   late double barHeight;
+  double currentBarHeight = 50;
+  @override
+  State<BarModel> createState() => _BarModelState();
+}
+
+class _BarModelState extends State<BarModel> {
+  @override
+  void initState() {
+    Timer(Duration(milliseconds: 500), () {
+      setState(() {
+        widget.currentBarHeight = widget.barHeight;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Container(
+        AnimatedContainer(
+          curve: Curves.easeInOutCubic,
+          duration: Duration(seconds: 1),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
             color: Colors.white70,
           ),
           width: Dimensions.width * .06,
-          height: barHeight,
+          height: widget.currentBarHeight,
         ),
         SizedBox(
           height: 8,
         ),
         Text(
-          barText,
+          widget.barText,
           style: TextStyle(fontWeight: FontWeight.w600),
         )
       ],
