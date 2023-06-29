8aac1b79cd5491ca09ac320475f5b71d5df5f313
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-06-29 15:16:08 +0300

    fvm deleted

--- green_egypt/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ green_egypt/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/amgad/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/amgad/Desktop/GE_Eco_System/green_egypt"
+export "FLUTTER_ROOT=C:\flutter_windows_3.10.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mavic\OneDrive\Desktop\GE_Eco_System\green_egypt"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
