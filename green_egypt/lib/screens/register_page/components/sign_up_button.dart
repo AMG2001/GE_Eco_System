a685a6f8bd401a9e40709dc42fb79204b8560365
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-08-19 02:59:06 +0500

    register page code modified

--- green_egypt/lib/screens/register_page/components/sign_up_button.dart
+++ green_egypt/lib/screens/register_page/components/sign_up_button.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:green_egypt/screens/register_page/controller/register_page_controller.dart';
 import 'package:green_egypt/services/custom_toast.dart';
 import 'package:green_egypt/services/email_otp_verification.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class SignUpButton extends StatelessWidget {
   final registerPageController = Get.put(RegisterPageController());
@@ -25,49 +26,70 @@ class SignUpButton extends StatelessWidget {
   final TextEditingController lastNameController;
   final TextEditingController userNumberController;
   final String userCredintial;
-
+  Widget widgetOnButton = const Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Text("Register now"),
+      SizedBox(
+        width: 10,
+      ),
+      FaIcon(
+        FontAwesomeIcons.arrowRight,
+        size: 16,
+      )
+    ],
+  );
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.centerRight,
-      child: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
-        child: const Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("Register now"),
-            SizedBox(
-              width: 10,
-            ),
-            FaIcon(
-              FontAwesomeIcons.arrowRight,
-              size: 16,
-            )
-          ],
-        ),
-        onPressed: () async {
-          if (sigUpFormState.currentState!.validate() && userCredintial != "") {
-            EmailOTPVerification.instance
-                .sendEmail(email: emailController.text)
-                .then(
-                  (value) => registerPageController.showOTPSheet(
-                      context: context,
-                      userNumberController: userNumberController,
-                      emailController: emailController,
-                      firstNameController: firstNameController,
-                      lastNameController: lastNameController,
-                      passwordController: passwordController,
-                      userCredintial: userCredintial),
-                );
-          } else {
-            CustomToast.showBlackToast(
-                messsage:
-                    "user credintial = ${registerPageController.userCredintial}");
-            CustomToast.showRedToast(messsage: "error in validation");
-          }
-        },
-      ),
-    );
+        alignment: Alignment.centerRight,
+        child: StatefulBuilder(
+          builder: (context, setState) {
+            return ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green)),
+              child: widgetOnButton,
+              onPressed: () async {
+                setState(() {
+                  // change the UI of the Regsiter button
+                  widgetOnButton = Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Transform.scale(
+                        scale: .5,
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  );
+                  // send otp request .
+                  if (sigUpFormState.currentState!.validate() &&
+                      userCredintial != "") {
+                    EmailOTPVerification.instance
+                        .sendEmail(email: emailController.text)
+                        .then(
+                          (value) => registerPageController.showOTPSheet(
+                              context: context,
+                              userNumberController: userNumberController,
+                              emailController: emailController,
+                              firstNameController: firstNameController,
+                              lastNameController: lastNameController,
+                              passwordController: passwordController,
+                              userCredintial: userCredintial),
+                        );
+                  } else {
+                    CustomToast.showBlackToast(
+                        messsage:
+                            "user credintial = ${registerPageController.userCredintial}");
+                    CustomToast.showRedToast(messsage: "error in validation");
+                  }
+                });
+              },
+            );
+          },
+        ));
   }
 }
